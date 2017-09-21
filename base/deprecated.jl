1eb0827c0b7d898673ddf3932ebcd5a9602f8534
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-09-21 18:59:11 -0400

    require explicit predicates in `find` functions
    fix #23120, fix #19186

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1855,6 +1855,14 @@ end
     nothing
 end
 
+@deprecate find(x::Number)            find(!iszero, x)
+@deprecate findnext(A, v, i::Integer) findnext(equalto(v), A, i)
+@deprecate findfirst(A, v)            findfirst(equalto(v), A)
+@deprecate findprev(A, v, i::Integer) findprev(equalto(v), A, i)
+@deprecate findlast(A, v)             findlast(equalto(v), A)
+# also remove deprecation warnings in find* functions in array.jl, sparse/sparsematrix.jl,
+# and sparse/sparsevector.jl.
+
 # END 0.7 deprecations
 
 # BEGIN 1.0 deprecations
